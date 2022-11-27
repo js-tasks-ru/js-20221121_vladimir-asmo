06e5c648ace3ca3b4e22a2ec986419b8aafdcdb4
Kobylinsky Vladimir <toward.vladimir@gmail.com>
2022-11-27 16:19:16 +0300

    feat (lect2): implement sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,20 @@
 /**
+ * compare
+ * @param {string} str1
+ * @param {string} str2
+ * @param {'upper'|'lower'} [caseFirst='upper']
+ * @returns number
+ */
+const compare = (str1, str2, caseFirst = 'upper') =>
+  str1.localeCompare(str2, ['ru', 'en'], { caseFirst });
+
+/**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
-}
+export const sortStrings = (arr, param = 'asc') =>
+  [...arr].sort((a, b) =>
+    param === 'asc' ? compare(a, b) : compare(b, a, 'lower')
+  );
