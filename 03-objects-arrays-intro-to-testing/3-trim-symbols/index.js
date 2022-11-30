a596ebb62535cb2afe55f87a8dd8783f89af3f18
Kobylinsky Vladimir <toward.vladimir@gmail.com>
2022-11-30 17:40:29 +0300

    feat (lect3): base implement trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,20 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
+  if (!size) {
+    return string;
+  }
 
+  let result = '';
+
+  for (const char of string) {
+    if (!result.endsWith(char.repeat(size))) {
+      result += char;
+    }
+  }
+
+  return result;
 }
